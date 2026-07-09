962538d21ae818fe484d9625bc20d974327217b6
Kxko <haleannson@gmail.com>
2026-07-10 01:50:05 +0200

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,9 @@
+const SUPABASE_URL = "https://ggewdpazpeoielbqrcgm.supabase.co";
 
+const SUPABASE_KEY = "sb_publishable_RZYBKLqjC9UYLNdqlOKZCA_IFX-5Gvm";
+
+
+const db = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
