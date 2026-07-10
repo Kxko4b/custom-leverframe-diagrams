4e108a096ca5acfdbde3179392670bde1bd986bd
Kxko <haleannson@gmail.com>
2026-07-10 11:25:08 +0200

    Update content.js

--- js/content.js
+++ js/content.js
@@ -1,1 +1,46 @@
+async function loadContent(){
 
+    const {data,error} =
+    await db
+    .from("site_content")
+    .select("*");
+
+
+    if(error){
+
+        console.error(error);
+        return;
+
+    }
+
+
+    data.forEach(item=>{
+
+
+        if(item.section === "about"){
+
+            document
+            .getElementById("about-text")
+            .innerHTML = item.content;
+
+        }
+
+
+
+        if(item.section === "terms"){
+
+            document
+            .getElementById("terms-text")
+            .innerHTML = item.content;
+
+        }
+
+
+    });
+
+
+}
+
+
+
+loadContent();
